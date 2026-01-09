17bdcc93b73e4bef2f86cc4f91cb16353811d04f
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-01-09 02:24:21 -0300

    feat(TransactionController): add getAllTransactions function

--- src/main/java/picpay/picpay/controllers/TransactionController.java
+++ src/main/java/picpay/picpay/controllers/TransactionController.java
@@ -1,8 +1,10 @@
 package picpay.picpay.controllers;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,16 +20,23 @@ import picpay.picpay.services.transactions.TransactionService;
 public class TransactionController {
   private final TransactionService service;
 
-  public TransactionController(TransactionService service) {
+  public TransactionController(
+    TransactionService service) {
     this.service = service;
   }
 
-  @PostMapping("{senderId}/{receiverId}")
-  public ResponseEntity<TransactionResponseDTO> createTransaction(@Valid @PathVariable String senderId,
-                                                                  @Valid @PathVariable String receiverId,
-                                                                  @Valid @RequestBody TransactionRequestDTO request) {
-    TransactionResponseDTO response = this.service.createTransaction(senderId, receiverId, request);
+  @PostMapping
+  public ResponseEntity<TransactionResponseDTO> createTransaction(
+    @Valid @RequestBody TransactionRequestDTO request) {
+    TransactionResponseDTO response = this.service.createTransaction(request);
 
-    return new ResponseEntity<>(response, HttpStatus.CREATED);
+    return ResponseEntity.status(HttpStatus.CREATED).body(response);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<TransactionResponseDTO>> getAllTransactions() {
+    List<TransactionResponseDTO> response = this.service.getAllTransactions();
+
+    return ResponseEntity.status(HttpStatus.OK).body(response);
   }
 }
