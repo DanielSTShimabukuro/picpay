3c6fb51c3f51befa32e91e9ca31324da667f640b
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-07-22 01:20:29 -0300

    feat(UserControllerTest): add shouldRegisterUserSuccefully function

--- src/test/java/picpay/picpay/controllers/UserControllerTest.java
+++ src/test/java/picpay/picpay/controllers/UserControllerTest.java
@@ -33,6 +33,31 @@ public class UserControllerTest {
   @Autowired
   private ObjectMapper objectMapper;
 
+  @Test
+  void shouldRegisterUserSuccessfully() throws Exception {
+    UserRegisterRequestDTO request = new UserRegisterRequestDTO("881.302.780-06", 
+                                                                    "daniel.s.t.shimabukuro@gmail.com", 
+                                                                    "Daniel", 
+                                                                    "Shimabukuro", 
+                                                                    "senha", 
+                                                                    BigDecimal.valueOf(1000), 
+                                                                    UserType.COMMON);
+    
+    UserResponseDTO response = this.buildUserResponseDTO();
+
+    when(this.service.registerUser(request)).thenReturn(response);
+
+    mockMvc.perform(post("/users").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.id").value(response.id()))
+            .andExpect(jsonPath("$.firstName").value(response.firstName()))
+            .andExpect(jsonPath("$.lastName").value(response.lastName()))
+            .andExpect(jsonPath("$.balance").value(response.balance()))
+            .andExpect(jsonPath("$.type").value(response.type().toString()));
+
+    verify(this.service).registerUser(request);
+  }
+
   private UserResponseDTO buildUserResponseDTO() {
     return new UserResponseDTO("51c92414-92cb-419a-92ec-63e166ab6a76", 
                                 "Daniel", 
